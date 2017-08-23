e8a018838a6d74bb30b35f2a994f52c0f913ca2b
양용기 <brave0681@gmail.com>
2017-08-24 04:36:13 +0900

    config 분리

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const Raven = require('raven');
-//const ravenConfig = require('./config.json');
+const ravenConfig = require('./config.json');
 
 const signContoller = require('./controllers/SignController');
 const newsFeedController = require('./controllers/NewsFeedController');
@@ -19,14 +19,14 @@ const storyController = require('./controllers/StoryController');
 
 const app = express();
 
-//Raven.config(ravenConfig.DSN).install();
+Raven.config(ravenConfig.DSN).install();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(Raven.requestHandler());
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(Raven.requestHandler());
 
 
 app.use('/sign', signContoller);
@@ -39,7 +39,7 @@ app.use('/story', storyController);
 
 
 
-// app.use(Raven.errorHandler());
+app.use(Raven.errorHandler());
 
 app.use(function(req, res, next) {
   let err = new Error('Not Found');
@@ -48,7 +48,6 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(err, req, res, next) {
-  console.log(err);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.sendStatus(err.status || 500);
--- models/index.js
+++ models/index.js
@@ -1,18 +1,14 @@
-var fs        = require("fs");
-var path      = require("path");
-var Sequelize = require("sequelize");
-var env       = process.env.NODE_ENV || "development";
-var sequelize = new Sequelize(
-  'allnew', // 데이터베이스 이름
-  'root', // 유저 명
-  'dyd0681', // 비밀번호
-  {
-    'host': 'localhost', // 데이터베이스 호스트
-    'port': 3306,
-    'dialect': 'mysql' // 사용할 데이터베이스 종류
-  }
-);
-var db        = {};
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const dbConfig = require('../config.json').db;
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+  'host': dbConfig.host,
+  'port': dbConfig.port,
+  'dialect': dbConfig.dialect
+});
+let db = {};
 
 fs
   .readdirSync(__dirname)
@@ -20,7 +16,7 @@ fs
     return (file.indexOf(".") !== 0) && (file !== "index.js");
   })
   .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
+    let model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
